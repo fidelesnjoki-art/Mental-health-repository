a90dbf5d6f00eaf282af4fb460c6d91fb525e8a2
fidelis <fideles.njoki@student.moringaschool.com>
2026-04-19 17:45:17 +0300

    Initial project setup with HTML structure

---
+++ Index.js
@@ -0,0 +1,19 @@
+async function getQuote() {
+  try {
+    let response = await fetch("https://api.quotable.io/random");
+    let data = await response.json();
+
+    document.getElementById("Quote").innerText =
+      data.content + " — " + data.author;
+  } catch (error) {
+    document.getElementById("Quote").innerText ="Stay strong. You are not alone ";
+  }
+}
+
+
+// Load on page open
+getQuote();
+
+if (document.getElementById("quote")) {
+  getQuote();
+}
---
+++ test.js
@@ -0,0 +1,11 @@
+function testGetResponse() {
+    let result = getResponse("I feel sad");
+
+    if (result.includes("sorry")) {
+        console.log("Test Passed");
+    }
+    else {
+        console.log("Test Failed");
+    }
+}
+testGetResponse();
